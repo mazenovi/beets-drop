3ec4f77ab23e24e9e8af3db41d9e8088da807012
mazenovi <vmazenod@gmail.com>
2016-06-01 10:57:47 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='Vincent Mazenod',
     author_email='vmazenod@gmail.com',
     description='Beets plugin to drop incomplete and unclassified items in particular folders',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     url='https://github.com/mazenovi/beets-drop',
     install_requires=[
         'beets'
