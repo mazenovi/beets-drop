195480328a88def0a1a86f8ac7ed5042027a01fb
mazenovi <vmazenod@gmail.com>
2016-06-01 10:18:29 +0200

    initial commit

---
+++ beetsplug/drop.py
@@ -0,0 +1,35 @@
+from beets.plugins import BeetsPlugin
+from beets.ui.commands import PromptChoice
+from beets import importer
+import shutil
+import os
+
+class DropPlugin(BeetsPlugin):
+    def __init__(self):
+        super(DropPlugin, self).__init__()
+        self.register_listener('before_choose_candidate',
+                               self.before_choose_candidate_event)
+
+    def before_choose_candidate_event(self, session, task):
+        return [PromptChoice('n', 'iNcomplete', self.incomplete),
+            PromptChoice('c', 'unClassified', self.unclassified)]
+
+    def incomplete(self, session, task):
+        #self.import_stages = [self.move_folder_to_incomplete]
+        self.move_folder_to_incomplete(session, task)
+        return importer.action.SKIP
+
+    def unclassified(self, session, task):
+        #self.import_stages = [self.move_folder_to_unclassified]
+        self.move_folder_to_unclassified(session, task)
+        return importer.action.SKIP
+
+    def move_folder_to_incomplete(self, session, task):
+        for p in task.paths:
+            shutil.move(p,
+                self.config['incomplete'].get() + p[len(task.toppath):len(p)])
+
+    def move_folder_to_unclassified(self, session, task):
+        for p in task.paths:
+            shutil.move(p,
+                self.config['unclassified'].get() + p[len(task.toppath):len(p)])
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='beets-drop',
+    version='0.0.1',
+    namespace_packages=['beetsplug'],
+    packages=['beetsplug'],
+    author='Vincent Mazenod',
+    author_email='vmazenod@gmail.com',
+    description='Beets plugin to drop incomplete and unclassified items in particular folders',
+    long_description=open('README.md').read(),
+    url='https://github.com/mazenovi/beets-drop',
+    install_requires=[
+        'beets'
+    ]
+)
